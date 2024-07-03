bb77efa2d077d362cf8c60ee1ad9f076fc093a1e
ramdan <72304741+elstrom@users.noreply.github.com>
2024-07-03 20:59:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -64,11 +64,6 @@ def git_commit(file_path):
         # Initialize the Git repository
         subprocess.run(["git", "init"], check=True)
         
-        # Check if the remote 'origin' already exists
-        result_remote = subprocess.run(["git", "remote"], capture_output=True, text=True)
-        if "origin" not in result_remote.stdout:
-            subprocess.run(["git", "remote", "add", "origin", "https://github.com/elstrom/SundaGenius"], check=True)
-        
         # Configure user details
         subprocess.run(["git", "config", "user.name", "elstrom"], check=True)
         subprocess.run(["git", "config", "user.email", "danram162@gmail.com"], check=True)
@@ -80,7 +75,7 @@ def git_commit(file_path):
         st.write(result_add.stdout)
         result_commit = subprocess.run(["git", "commit", "-m", commit_message], check=True, capture_output=True, text=True)
         st.write(result_commit.stdout)
-        result_push = subprocess.run(["git", "push", "-u", "origin", "master"], check=True, capture_output=True, text=True)
+        result_push = subprocess.run(["git", "push", "-u", "master"], check=True, capture_output=True, text=True)
         st.write(result_push.stdout)
     except subprocess.CalledProcessError as e:
         logging.error(f"Error during git operation: {e.stderr}")
