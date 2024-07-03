7dae02b9ad8cef1dc996d09827a658a85f2fe775
ramdan <72304741+elstrom@users.noreply.github.com>
2024-07-03 22:22:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ def git_commit(file_path):
         # Menggunakan token akses pribadi untuk push
         username = "elstrom"
         token = "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"
-        repo_url = f"https://{username}:{token}@github.com/elstrom/SundaGenius.git"
+        repo_url = f"https://{token}@github.com/elstrom/SundaGenius.git"
         result_push = subprocess.run(["git", "push", repo_url, "main"], check=True, capture_output=True, text=True)
         st.write(result_push.stdout)
         st.write("Pushed to GitHub successfully")
